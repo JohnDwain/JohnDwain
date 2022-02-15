cacecd46e46b921d9a25481aa3bd61e7e6da7e9e
johnn <johnnydanhle@gmail.com>
2022-02-15 16:43:35 +0100

    new learnings added
    JL

---
+++ learning/src/expressions.java
@@ -0,0 +1,17 @@
+public class expressions {
+    public static void main(String[] args) {
+
+        // expression = operands & operators
+        // operands = values, variables, numbers, quantity
+        // operators = + - * / %
+
+        double friends = 10;
+
+        // friends = friends % 2;  modulant
+        // friends++; adding plus 1
+        // friends = (int) friends / 3; converting double to integer
+
+        System.out.println(friends);
+
+    }
+}
